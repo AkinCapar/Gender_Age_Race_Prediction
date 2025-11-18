a25ef75136d7b1bd2e286d7a6c907d4171c55a1e
Akin Çapar <akincapar2@gmail.com>
2025-11-18 07:52:14 +0100

    gradio legacy api mode is deleted.

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ interface = gr.Interface(
     outputs=gr.Textbox(),
     title="Gender, Age & Race Prediction",
     description="Upload an face image and get predictions. Please only upload .jpg files.",
-    allow_flagging="never",
-    api_mode="legacy"
+    allow_flagging="never"
 )
 
 if __name__ == "__main__":
